b93ea2b2106480e907c61d72b3b5bebcfb83dc70
asilva <adriano@gescob.com.br>
2023-03-27 12:35:49 -0300

    Validação de token da sessao usuario se retorno null

--- app/Filters/Auth.php
+++ app/Filters/Auth.php
@@ -2,6 +2,7 @@
 
 namespace App\Filters;
 
+use App\Entities\SessaoUsuarioEntity;
 use App\Libraries\JwtToken;
 use App\Libraries\Uuid;
 use CodeIgniter\HTTP\RequestInterface;
@@ -23,6 +24,14 @@ class Auth implements FilterInterface
         if (empty($token)) return $response->setStatusCode(401, "Falha ao validar token")->setJSON(['status' => false, 'msg' => "Falha ao validar token da sessão!"]);
 
         if (!Uuid::is_valid($token['tokenUsuario'])) return $response->setStatusCode('401', 'Token inválido!')->setJSON(['status' => false, 'msg' => "Token do usuario inválido!"]);
+
+        $sessaoUsuario = new SessaoUsuarioEntity();
+
+        $sessaoUsuario = $sessaoUsuario->buscarDadosSessaoUsuario($jwt);
+
+        if (empty($sessaoUsuario->__get('usuario')->__get('empresa')->__get('emp_id'))) {
+            return $response->setStatusCode(401, 'Não Autorizado')->setJSON(['status' => false, 'msg' => "Usuario não encontrado!"]);
+        }
     }
 
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
