d94977079d3be286c528a50bbd49809cc5b808e8
asilva <adriano@gescob.com.br>
2023-07-23 15:17:37 -0300

    Retirado o campo de tipo de pagamento da tabela de vendas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -163,11 +163,6 @@ $routes->group('api', static function ($routes) {
         $routes->patch('alterar_status', 'TipoPagamentoController::alterarStatusTipoPagamento', ['filter' => 'auth']);
         $routes->get('listar', 'TipoPagamentoController::buscaListaTipoPagamento', ['filter' => 'auth']);
     });
-
-    $routes->group('servico', static function ($routes) {
-        $routes->post('forma_pagamento_padrao', 'ServicoController::criarFormasPagamentoPadraoEmpresas');
-        $routes->post('correcao_forma_pagamento_venda', 'ServicoController::corrigiFormasPagamentoVendasExistente');
-    });
 });
 
 /*
